60d18a5b90dde1c6658881456e4ffa2a5374d3f5
FlareItsh <rayverdasalla@gmail.com>
2026-07-21 21:29:26 +0800

    feat: add role column to users table with default value

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
-#[Fillable(['uuid', 'nickname', 'password'])]
+#[Fillable(['uuid', 'nickname', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
